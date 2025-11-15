ce91b97daf189ea9a945eee316fba0d1963734ae
Yang Ji <yj13138420804@163.com>
2025-11-15 10:15:05 +0800

    CN.2549097115426044:0a1343ad0ebec50843fde79c41b1288d_6917e15d27ac31bda562353f.6917e17427ac31bda5623543.6917e173834b1c221de118fb:Trae CN.T(2025/11/15 10:12:04)

--- src/LFU.java
+++ src/LFU.java
@@ -1,67 +1,152 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeSet;
 
 class LFUCache {
-    // 缓存容量，时间戳
-    int capacity, time;
-    Map<Integer, Node> key_table;
-    TreeSet<Node> S;
+    // 节点类
+    static class Node {
+        int key, value, freq;
+        Node prev, next;
+        
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+            this.freq = 1;
+        }
+    }
+    
+    // 频率链表类
+    static class FreqList {
+        int freq;
+        Node head, tail;
+        int size;
+        
+        FreqList(int freq) {
+            this.freq = freq;
+            this.head = new Node(0, 0);
+            this.tail = new Node(0, 0);
+            head.next = tail;
+            tail.prev = head;
+            this.size = 0;
+        }
+        
+        // 在链表头部插入节点（最新访问）
+        void addFirst(Node node) {
+            Node next = head.next;
+            head.next = node;
+            node.prev = head;
+            node.next = next;
+            next.prev = node;
+            size++;
+        }
+        
+        // 删除指定节点
+        void remove(Node node) {
+            Node prev = node.prev;
+            Node next = node.next;
+            prev.next = next;
+            next.prev = prev;
+            size--;
+        }
+        
+        // 删除最旧的节点（尾部）
+        Node removeLast() {
+            if (size == 0) return null;
+            Node last = tail.prev;
+            remove(last);
+            return last;
+        }
+        
+        boolean isEmpty() {
+            return size == 0;
+        }
+    }
+    
+    private int capacity;
+    private int minFreq;
+    private Map<Integer, Node> keyNodeMap;
+    private Map<Integer, FreqList> freqListMap;
 
     public LFUCache(int capacity) {
         this.capacity = capacity;
-        this.time = 0;
-        key_table = new HashMap<Integer, Node>();
-        S = new TreeSet<Node>();
+        this.minFreq = 0;
+        this.keyNodeMap = new HashMap<>();
+        this.freqListMap = new HashMap<>();
     }
 
     public int get(int key) {
-        if (capacity == 0) {
+        if (capacity == 0 || !keyNodeMap.containsKey(key)) {
             return -1;
         }
-        // 如果哈希表中没有键 key，返回 -1
-        if (!key_table.containsKey(key)) {
-            return -1;
-        }
-        // 从哈希表中得到旧的缓存
-        Node cache = key_table.get(key);
-        // 从平衡二叉树中删除旧的缓存
-        S.remove(cache);
-        // 将旧缓存更新
-        cache.cnt += 1;
-        cache.time = ++time;
-        // 将新缓存重新放入哈希表和平衡二叉树中
-        S.add(cache);
-        key_table.put(key, cache);
-        return cache.value;
+        
+        Node node = keyNodeMap.get(key);
+        // 更新节点频率
+        increaseFreq(node);
+        
+        return node.value;
     }
 
     public void put(int key, int value) {
         if (capacity == 0) {
             return;
         }
-        if (!key_table.containsKey(key)) {
-            // 如果到达缓存容量上限
-            if (key_table.size() == capacity) {
-                // 从哈希表和平衡二叉树中删除最近最少使用的缓存
-                key_table.remove(S.first().key);
-                S.remove(S.first());
-            }
-            // 创建新的缓存
-            Node cache = new Node(1, ++time, key, value);
-            // 将新缓存放入哈希表和平衡二叉树中
-            key_table.put(key, cache);
-            S.add(cache);
+        
+        if (keyNodeMap.containsKey(key)) {
+            // 键已存在，更新值和频率
+            Node node = keyNodeMap.get(key);
+            node.value = value;
+            increaseFreq(node);
         } else {
-            // 这里和 get() 函数类似
-            Node cache = key_table.get(key);
-            S.remove(cache);
-            cache.cnt += 1;
-            cache.time = ++time;
-            cache.value = value;
-            S.add(cache);
-            key_table.put(key, cache);
+            // 键不存在，需要插入新节点
+            if (keyNodeMap.size() >= capacity) {
+                // 容量已满，删除最不常用且最旧的节点
+                FreqList minFreqList = freqListMap.get(minFreq);
+                Node removedNode = minFreqList.removeLast();
+                keyNodeMap.remove(removedNode.key);
+                
+                // 如果该频率链表为空，移除该频率
+                if (minFreqList.isEmpty()) {
+                    freqListMap.remove(minFreq);
+                }
+            }
+            
+            // 创建新节点
+            Node newNode = new Node(key, value);
+            keyNodeMap.put(key, newNode);
+            
+            // 将新节点加入频率为1的链表
+            FreqList freq1List = freqListMap.getOrDefault(1, new FreqList(1));
+            freq1List.addFirst(newNode);
+            freqListMap.put(1, freq1List);
+            
+            // 新节点的频率为1，所以minFreq重置为1
+            minFreq = 1;
         }
+    }
+    
+    // 增加节点的频率
+    private void increaseFreq(Node node) {
+        int oldFreq = node.freq;
+        FreqList oldFreqList = freqListMap.get(oldFreq);
+        
+        // 从旧频率链表中移除节点
+        oldFreqList.remove(node);
+        
+        // 如果旧频率链表为空且是当前最小频率，更新minFreq
+        if (oldFreqList.isEmpty()) {
+            freqListMap.remove(oldFreq);
+            if (oldFreq == minFreq) {
+                minFreq++;
+            }
+        }
+        
+        // 更新节点频率
+        int newFreq = oldFreq + 1;
+        node.freq = newFreq;
+        
+        // 将节点加入新频率链表
+        FreqList newFreqList = freqListMap.getOrDefault(newFreq, new FreqList(newFreq));
+        newFreqList.addFirst(node);
+        freqListMap.put(newFreq, newFreqList);
     }
     public static void main(String[] args) {
         // 测试用例1: 基本功能测试
